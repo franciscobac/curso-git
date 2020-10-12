67167485796dc0704095aa7a1c8b59b00a884fc8
franciscobac <franciscobac@hotmail.com>
2020-10-12 17:26:05 -0300

    Acrescentada a mensagem desejando um bom dia

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ namespace C_sharp {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá Mundo");
-            System.Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!")
         }
     }
 }
