f401cae09f7d0498568b14b583ef6e0e71a8021e
franciscobac <franciscobac@hotmail.com>
2020-10-12 16:32:17 -0300

    Acrescentada uma mensagem de bom dia

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace C_sharp {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá Mundo");
+            System.Console.WriteLine("Bom dia");
         }
     }
 }
