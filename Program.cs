e2c1e59abd6a7272ab02623ece2817fbff1ad6b1
franciscobac <franciscobac@hotmail.com>
2020-10-13 01:39:44 -0300

    Acrescentando tracos

--- Program.cs
+++ Program.cs
@@ -3,10 +3,12 @@
 namespace C_sharp {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("---------------------------------------");
             Console.WriteLine("Olá Mundo");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("---------------------------------------");
         }
     }
 }
