da7b988b1c033877ec1dc4c585a4ae28bc5c0091
franciscobac <franciscobac@hotmail.com>
2020-10-13 01:24:18 -0300

    Acrescentada a mensagem de despedida

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ namespace C_sharp {
         static void Main(string[] args) {
             Console.WriteLine("Olá Mundo");
             Console.WriteLine("Como você está hoje?");
-            Console.WriteLine("Tenha um bom dia!")
+            Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau!");
         }
     }
 }
