34694c383efe54b7db22b7cf8ce0362b8d6f57a2
franciscobac <franciscobac@hotmail.com>
2021-07-29 00:06:16 -0300

    Exercicio sobre Herança e Polimorfismo

---
+++ Entities/132-Exercicio_Heranca_e_Polimorfismo_OutsourcedEmployee.cs
@@ -0,0 +1,21 @@
+﻿namespace ExerciciosCursoCSharp.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionCharge) :base(name, hours, valuePerHour)
+        {
+            AdditionCharge = additionCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionCharge;
+        }
+    }
+}
