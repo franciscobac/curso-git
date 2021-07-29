42e61dbd515256d5812223cac4a7e1f4893adbdb
franciscobac <franciscobac@hotmail.com>
2021-07-28 22:56:43 -0300

    Classes e métodos selados

---
+++ Entities/129-Classes_e_Metodos_Selados_SavingsAccounts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCursoCSharp.Entities
+{
+    sealed class SavingsAccounts : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccounts()
+        {
+        }
+
+        public SavingsAccounts(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
