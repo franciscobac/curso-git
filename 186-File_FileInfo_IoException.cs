9bb08ae764456b0f974af95f6fdf04daea6c78ad
franciscobac <franciscobac@hotmail.com>
2021-08-26 01:33:09 -0300

    Trabalhando com arquivos, File, FileInfo e IoException

---
+++ 186-File_FileInfo_IoException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExerciciosCursoCSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\File1.txt";
+            string targetPath = @"c:\temp\File2.txt";
+            string[] lines = File.ReadAllLines(sourcePath);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
