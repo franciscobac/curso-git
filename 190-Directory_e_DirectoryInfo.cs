c0dc2ae1e802090542b35a685d6b38435a793dd1
franciscobac <franciscobac@hotmail.com>
2021-08-28 17:17:56 -0300

    Aula sobre as Classes Directory e DirectoryInfo, trabalhando com diretórios

---
+++ 190-Directory_e_DirectoryInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ExerciciosCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                // Buscando diretórios
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                // Buscando arquivos
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                // Criando diretório
+                Directory.CreateDirectory(path + "\\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
