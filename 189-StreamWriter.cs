b7088fd6be5d996389386430c93f50570b6f0f84
franciscobac <franciscobac@hotmail.com>
2021-08-28 16:48:42 -0300

    Utilizando o StringWriter para a ecrita em arquivos

---
+++ 189-StreamWriter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExerciciosCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            string sourcePath = @"c:\temp\File1.txt";
+            string targetPath = @"c:\temp\File2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
