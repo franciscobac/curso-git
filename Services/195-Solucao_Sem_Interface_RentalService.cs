61b421a8efcefb6bc974301b8aa0f6ed3bf16ed8
franciscobac <franciscobac@hotmail.com>
2021-09-02 20:57:24 -0300

    Introdução sobre Interfaces. Resolvendo um problema com uma solução sem interface

---
+++ Services/195-Solucao_Sem_Interface_RentalService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ExerciciosCursoCSharp.Entities;
+
+namespace ExerciciosCursoCSharp.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay{ get; private set; }
+
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0.0;
+
+            if (duration.TotalHours <= 12.0)
+            {
+                // A função Math.Ceiling arrendonda o número fracionado para cima
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
