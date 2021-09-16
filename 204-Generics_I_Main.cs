95dad09761c0ede790e1f2f686c292c4afebb354
franciscobac <franciscobac@hotmail.com>
2021-09-16 00:31:57 -0300

    Generics Part I

---
+++ 204-Generics_I_Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
