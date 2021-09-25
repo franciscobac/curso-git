77a4827dedbad15bbb1e9d109f689742e87a9a84
franciscobac <franciscobac@hotmail.com>
2021-09-25 02:02:21 -0300

    Aula sobre GetHashCode e Equals

---
+++ 207-GetHashCodeAndEquals_Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExerciciosCursoCSharp.Entities;
+
+namespace ExerciciosCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alexgmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
