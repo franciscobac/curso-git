9a23c363c4518f6bc49fbeded4018d7df6705c6b
franciscobac <franciscobac@hotmail.com>
2021-09-28 00:27:45 -0300

    Exercício resolvido sobre conjuntos

---
+++ 211-ExercicioResolvido_Conjuntos_Main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExerciciosCursoCSharp.Entities;
+
+namespace ExerciciosCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+
+                    Console.Write("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
