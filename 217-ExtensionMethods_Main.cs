c1ac8c70bc4bf6c882d91053bfb734049532bc57
franciscobac <franciscobac@hotmail.com>
2021-10-05 02:14:43 -0300

    Aula sobre Extension Methods

---
+++ 217-ExtensionMethods_Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 10, 01, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            String s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
