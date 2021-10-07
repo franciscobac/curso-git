661c940943e38ce01a11b5ea37caacbab965b4b5
franciscobac <franciscobac@hotmail.com>
2021-10-06 23:42:22 -0300

    Aula sobre multi delegates

---
+++ 222-Multi_delegates_Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExerciciosCursoCSharp.Services;
+
+namespace ExerciciosCursoCSharp
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            op(a, b);
+        }
+    }
+
+}
