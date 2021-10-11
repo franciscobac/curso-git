a79128198c6f250cb8d3382f435a316e4b82115b
franciscobac <franciscobac@hotmail.com>
2021-10-11 18:36:06 -0300

    Exercicio resolvido LINQ

--- Entities/232-Exercicio_LINQ_Product.cs
+++ Entities/232-Exercicio_LINQ_Product.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace ExerciciosCursoCSharp.Entities
 {
-    class Category
+    class Product
     {
-        public int Id { get; set; }
         public string Name { get; set; }
-        public int Tier { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
 
 
     }
